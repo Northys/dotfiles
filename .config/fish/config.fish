ba6ff85c65e81bae8d28b1aa82cac6af5eae7c80
Jiri Travnicek <jiri.travnicek@nubium.cz>
2016-12-25 06:02:22 +0100

    .config directory

---
+++ .config/fish/config.fish
@@ -0,0 +1,4 @@
+set -gx PATH /usr/local/sbin /usr/local/bin $PATH
+set -gx LANG en_US.UTF-8
+set -gx LC_COLLATE en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/Users/northys/.local/share/omf/themes/cbjohnson/fish_prompt.fish
